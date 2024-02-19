634cd664c9845a7b9612a6aa72f64e1591a22648
Aarav <aarav.narayan@gmail.com>
2024-02-19 11:58:07 -0500

    styling+icon init, homepage

---
+++ components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+          <IconSettings />
+        </div>
+      </div>
+      <div class="navbar-center">
+        <a class="btn btn-ghost text-xl">Codebusters Prep</a>
+      </div>
+      <div class="navbar-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+          <IconCircleUser />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-6xl font-bold">Hello there</h1>
+          <button class="btn btn-primary m-6">Play Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
